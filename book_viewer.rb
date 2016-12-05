2deda9e1db6e37c7276a12ac8e7a33b5dc6f58de
Elizabeth Tackett <emctackett@gmail.com>
2016-12-05 12:14:03 -0600

    book viewer project LS 170

---
+++ book_viewer.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  @title = "The Adventure of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
+
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
+end
